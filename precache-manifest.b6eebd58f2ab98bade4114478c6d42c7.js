4a98a2aebef32fff03d90b6d197021c95c31bef8
Santos Gonzalez <sgonzo3@gmail.com>
2019-03-26 18:30:51 -0400

    Updates

---
+++ precache-manifest.b6eebd58f2ab98bade4114478c6d42c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bookshelf-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e1114cc2891f6f8003b",
+    "url": "/Bookshelf-App/static/js/runtime~main.28372b76.js"
+  },
+  {
+    "revision": "5447c44fe631652f0eaf",
+    "url": "/Bookshelf-App/static/js/main.414e1c78.chunk.js"
+  },
+  {
+    "revision": "5a8ca778a3bdb48dac0f",
+    "url": "/Bookshelf-App/static/js/2.f953cf9b.chunk.js"
+  },
+  {
+    "revision": "5447c44fe631652f0eaf",
+    "url": "/Bookshelf-App/static/css/main.e9446951.chunk.css"
+  },
+  {
+    "revision": "b66c37cf55eb662ea67bf5abaf1a0b78",
+    "url": "/Bookshelf-App/index.html"
+  }
+];
